4b30500eaa9467289c768e8e980d0828bc244e2f
ZoneTwelve <zonetwelve159@gmail.com>
2020-01-15 20:07:35 +0800

    [add] footer && dominant hue table

--- src/js/main.animation.js
+++ src/js/main.animation.js
@@ -1,3 +1,38 @@
+window.onload = function(){
+
+}
+
+function stickyScrolling(){
+  // Custom 
+  var stickyToggle = function(sticky, stickyWrapper, scrollElement) {
+    var stickyHeight = sticky.outerHeight();
+    var stickyTop = stickyWrapper.offset().top;
+    if (scrollElement.scrollTop() >= stickyTop){
+      stickyWrapper.height(stickyHeight);
+      sticky.addClass("is-sticky");
+    }
+    else{
+      sticky.removeClass("is-sticky");
+      stickyWrapper.height('auto');
+    }
+  };
+  
+  // Find all data-toggle="sticky-onscroll" elements
+  $('[data-toggle="sticky-onscroll"]').each(function() {
+    var sticky = $(this);
+    var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
+    sticky.before(stickyWrapper);
+    sticky.addClass('sticky');
+    
+    // Scroll & resize events
+    $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function() {
+      stickyToggle(sticky, stickyWrapper, $(this));
+    });
+    
+    stickyToggle(sticky, stickyWrapper, $(window));
+  });
+}
+
 function hamburgerMenuAnimation(e) {
   e.classList.toggle("change");
 }
